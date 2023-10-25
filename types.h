03bc6a6e4155ecb15ad3262a061ef82bfdb745fc
KapPetrov <148530033+KapPetrov@users.noreply.github.com>
2023-10-25 17:46:14 +0530

    Delete types.h

--- types.h
+++
@@ -1,9 +0,0 @@
-
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef          int   s32int;
-typedef          short s16int;
-typedef          char  s8int;
-
-#define NULL 0
